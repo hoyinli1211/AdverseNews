55006602271ae66a5955e67ec5918e974de08780
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-14 00:17:28 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 import streamlit as st
+
+import requests
+from bs4 import BeautifulSoup
+
+def read_content(urls):
+    contents = []
+    for url in urls:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        content = soup.find("div", {"id": "article-body"}).text
+        contents.append(content)
+    return contents
 
 def perform_search(name):
     search_url = f"https://www.google.com/search?q={name}+adverse+news"
@@ -11,12 +21,15 @@ def perform_search(name):
 
     # Extract the title and link from the search results
     data = []
+    urls = []
     for result in results[:5]:
         title = result.find("h3").text
         link = result.find("a")["href"]
         data.append({"Title": title, "Link": link})
-
-    # Create a DataFrame from the extracted data
+        urls.append(link)
+    contents = read_content(urls)
+    for i in range(5):
+        data[i].update({"content": contents[i]})
     df = pd.DataFrame(data)
     return df
     
